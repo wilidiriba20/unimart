eaa45fd1fb11adfa78ebb29ef62cdc8f98d34bae
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-03-28 00:28:04 +0300

    final

--- products/models.py
+++ products/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.conf import settings   # ✅ ADD THIS
-
+from cloudinary.models import CloudinaryField
 class Product(models.Model):
 
     CONDITION_CHOICES = [
@@ -14,7 +14,7 @@ class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/')
+    image = CloudinaryField('image')
     category = models.CharField(max_length=100)
     condition = models.CharField(max_length=20, choices=CONDITION_CHOICES)
 
--- unimart/settings.py
+++ unimart/settings.py
@@ -111,6 +111,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# =========================
+# MEDIA
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 # =========================
 # MEDIA (CLOUDINARY)
 # =========================
