055626e398a170bb0a0b5763771ab233cf70c163
wilidiriba20 <157874486+wilidiriba20@users.noreply.github.com>
2026-03-27 21:55:56 +0300

    railway

--- unimart/settings.py
+++ unimart/settings.py
@@ -109,7 +109,7 @@ DATABASES = {
 # STATIC FILES
 # =========================
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR.parent / "static"]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
